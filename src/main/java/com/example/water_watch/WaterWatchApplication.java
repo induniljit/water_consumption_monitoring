49ccca9488141ddd52adc697d373d5c5ecb6d3bc
induniljit <induniljit@gmail.com>
2023-07-13 11:50:07 +0530

    first commit

---
+++ src/main/java/com/example/water_watch/WaterWatchApplication.java
@@ -0,0 +1,14 @@
+package com.example.water_watch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterWatchApplication.class, args);
+		System.out.println("Hello indunil Well come to Java World");
+	}
+
+}
---
+++ src/test/java/com/example/water_watch/WaterWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.water_watch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
